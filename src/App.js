8d178f1b8e0526cf25f2934fec70ee3186d5987e
AntonVew <141330983+AntonVew@users.noreply.github.com>
2024-04-30 16:32:29 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import PostList from "./components/PostList";
 import PostDetails from "./components/PostDetails";
 import NewPost from "./components/NewPost";
-import { Route, Routes } from 'react-router-dom';
-import AccountPage from './components/AccountPage';
 
 function App() {
   return (
@@ -12,7 +10,6 @@ function App() {
       <Switch>
         <Route exact path="/" component={PostList} />
         <Route path="/post/:postId" component={PostDetails} />
-        <Route path="/account" element={<AccountPage />} />
       </Switch>
       <NewPost />
     </Router>
